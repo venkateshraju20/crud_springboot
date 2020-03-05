573fb35f1bafc913fd42f0c7a22d68012e5bd7a3
Venkatesh Raju <venkatesh.raju@unitsec.com>
2020-03-05 12:50:28 +0530

    modified pom xml

--- src/main/java/code/boot/SpringbootCrudApplication.java
+++ src/main/java/code/boot/SpringbootCrudApplication.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootCrudApplication {
+public class SpringbootCrudApplication extends SpringBootServletInitializer {
+
 	private static final Logger logger = LoggerFactory.getLogger(SpringbootCrudApplication.class);
 
 	public static void main(String[] args) {
