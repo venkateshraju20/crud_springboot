6f977cf8879bf671fe4653177d6d8e6bbae90321
Venkatesh Raju <venkatesh.raju@unitsec.com>
2020-03-10 18:26:24 +0530

    added sample files

--- src/main/java/code/boot/websecurity/WebSecurityConfig.java
+++ src/main/java/code/boot/websecurity/WebSecurityConfig.java
@@ -2,6 +2,9 @@ package code.boot.websecurity;
 
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,42 +14,56 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
-    public HttpFirewall looseHttpFirewall() {
-        StrictHttpFirewall firewall = new StrictHttpFirewall();
-        firewall.setAllowedHttpMethods(Arrays.asList("GET", "POST","PUT","CONNECT","HEAD", "DELETE"));
-//        firewall.setAllowSemicolon(true);
-//        firewall.setAllowUrlEncodedSlash(true);
-//        firewall.setAllowBackSlash(true);
-//        firewall.setAllowUrlEncodedPercent(true);
-//        firewall.setAllowUrlEncodedPeriod(true);
-        return firewall;
-    }
-	
+	public HttpFirewall looseHttpFirewall() {
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowedHttpMethods(Arrays.asList("GET", "POST", "PUT", "CONNECT", "HEAD", "DELETE", "OPTIONS"));
+
+		return firewall;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().loginProcessingUrl("/login").loginPage("/login.html").usernameParameter("email").and().csrf()
-				.disable().authorizeRequests().antMatchers("/api/user/**").permitAll()
-				.antMatchers("/api/user/**").permitAll().antMatchers("/api/user/**").permitAll()
-				.antMatchers("*.html").permitAll().anyRequest().authenticated().and().exceptionHandling()
-				.and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-		// http.addFilterBefore(authenticationJwtTokenFilter(),
-		// UsernamePasswordAuthenticationFilter.class);
+		http.formLogin().loginProcessingUrl("/login").loginPage("/login.html").usernameParameter("email").and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+					.and()
+				.authorizeRequests()
+					.antMatchers("/**/*.{js,html,css}").permitAll()
+					.antMatchers("/api/user/**").permitAll()
+					.anyRequest().authenticated().and().exceptionHandling().and()
+					.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
+	@Bean
+	public RequestCache refererRequestCache() {
+		return new HttpSessionRequestCache() {
+			@Override
+			public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+				String referrer = request.getHeader("referer");
+				if (referrer != null) {
+					request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
+							new SimpleSavedRequest(referrer));
+				}
+			}
+		};
+	}
+
 }
